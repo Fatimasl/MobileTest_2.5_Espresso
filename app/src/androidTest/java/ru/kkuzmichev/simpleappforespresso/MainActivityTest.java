5bcf9ef43c41c47f17f02fcce291e33f3528a696
Neto test <s.e.lebedeva74@gmail.com>
2024-11-11 00:08:38 +0500

    Initial commit

---
+++ app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/MainActivityTest.java
@@ -0,0 +1,37 @@
+package ru.kkuzmichev.simpleappforespresso;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.junit.Assert.assertEquals;
+import static java.util.regex.Pattern.matches;
+
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.espresso.assertion.ViewAssertions;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
+
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> activityTestRule =
+            new ActivityTestRule<>(MainActivity.class);
+
+    private String stText_home = "This is home fragment";
+
+    @Test
+    public void mainActivityTest() {
+        ViewInteraction mainText = onView(withId(R.id.text_home));
+        mainText.check(ViewAssertions.matches(isDisplayed()));
+        mainText.check(ViewAssertions.matches(withText(stText_home)));
+
+    }
+}
